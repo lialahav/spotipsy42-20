11929798173a7508d95e601ec773cd3c1e5e5c40
42 <42@gmail.com>
2026-03-17 18:39:26 +0200

    added layout

--- frontend_spotipsy/src/App.tsx
+++ frontend_spotipsy/src/App.tsx
@@ -1,55 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import type { Song } from "./assets/types.tsx";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import useStyles from './AppStyles';
+import Header from './components/Header/header';
+import Player from './components/Player/player';
+import Sidebar from './components/Sidebar/sidebar';
+import PageContent from './components/PageContent/pageContent';
 
-import './App.css'
+const App: React.FC = () => {
+  const { classes} = useStyles();
 
-const App :React.FC= () =>{
-  const [songsList, setSongsList] = useState<Song[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<string>();
-  //async function to fetch songs
-  const fetchSongs = async () => {
-    //start loading songs
-    setIsLoading(true);
-    try{
-      //access server
-      const response = await fetch("http://127.0.0.1:5001/api/songs")//todo:add url
-      const data = await response.json();
-      //add songs to songs list
-      setSongsList(data);
-    } 
-    catch (error){
-      //display error
-      setError("Someting went wrong");
-      console.error(error);
-      return;
-    }
-    finally {
-      // set end of songs loading
-      setIsLoading(false);
-    }
-  };
-  //load songs only at the first calling of the component
-  useEffect(() => {
-    fetchSongs();
-  }, []);
 
   return (
-    <div>
-      {/*display loading texst incase info still loading*/}
-      {isLoading && <p>Loading...</p>}
+    <ThemeProvider theme={theme}>
+      <div className={classes.mainContainer}>
+        <Header />
 
-      {/*display error*/}
-      {error && <p>{error}</p>}
-
-      {/*display songs*/}
-      {!isLoading && !error && songsList.map((song, index) =>(
-        <div key={index}>
-          <h2>{song.name}</h2>
+        <div className={classes.mainSection}>
+          <PageContent/>
+          <Sidebar />
         </div>
-      ))}
-    </div>
+
+        <Player />
+      </div>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
---
+++ frontend_spotipsy/src/components/PageContent/pageContent.tsx
@@ -0,0 +1,13 @@
+import useStyles from "./pageContentStyles";
+
+const PageContent: React.FC = () => {
+    const { classes } = useStyles();
+
+    return (
+        <div className={classes.container}>
+
+        </div>
+    )
+}
+
+export default PageContent;
