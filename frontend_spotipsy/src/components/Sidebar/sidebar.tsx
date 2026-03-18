d8b57fa119db648a3c7f2cbc743cb3957c16fc1b
42 <42@gmail.com>
2026-03-18 08:39:01 +0200

    Fixed app componenet+ added buttons

--- frontend_spotipsy/src/components/Sidebar/sidebar.tsx
+++ frontend_spotipsy/src/components/Sidebar/sidebar.tsx
@@ -1,4 +1,7 @@
 import useStyles from "./sidebarStyles";
+import HomeIcon from '@mui/icons-material/Home';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const Sidebar: React.FC = () => {
     const { classes } = useStyles();
@@ -6,7 +9,35 @@ const Sidebar: React.FC = () => {
 
     return (
         <div className={classes.container}>
+            <div className={classes.allSongsButton}>
+                <div className={classes.text}>
+                    <span>כל השירים</span>
+                </div>
 
+                <div className={classes.icon}>
+                    <HomeIcon />
+                </div>
+            </div>
+
+            <div className={classes.allSongsButton}>
+                <div className={classes.text}>
+                    <span>פלייליסטים</span>
+                </div>
+
+                <div className={classes.icon}>
+                    <LibraryMusicIcon />
+                </div>
+            </div>
+
+            <div className={classes.allSongsButton}>
+                <div className={classes.text}>
+                    <span>מועדפים</span>
+                </div>
+
+                <div className={classes.icon}>
+                    <FavoriteIcon />
+                </div>
+            </div>
         </div>
     )
 }
--- frontend_spotipsy/src/components/Sidebar/sidebarStyles.tsx
+++ frontend_spotipsy/src/components/Sidebar/sidebarStyles.tsx
@@ -2,13 +2,31 @@ import { makeStyles } from "tss-react/mui";
 
 const useStyles = makeStyles()({
     container: {
-        width: '20%',
+        width: '15%',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-start',
         alignItems: 'center',
         borderLeft: '1px solid rgba(49, 49, 49, 1)',
     },
+    allSongsButton: {
+        height: '7%',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        marginTop: '5px'
+    },
+    text: {
+        width: '70%',
+        display: 'flex',
+        justifyContent: 'flex-end'
+    },
+    icon: {
+        width: '30%',
+        display: 'flex',
+        justifyContent: 'center'
+    }
 
 });
 
