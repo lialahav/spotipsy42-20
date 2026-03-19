962ff33abbedf790a8cfa5ce42658f76c4355d07
lialahav <lialahav1@gmail.com>
2026-03-19 10:50:51 +0200

    not finished

--- frontend_spotipsy/src/components/Player/player.tsx
+++ frontend_spotipsy/src/components/Player/player.tsx
@@ -1,12 +1,40 @@
 import useStyles from "./playerStyles";
-
-const Player: React.FC = () => {
+import type { Song } from "../../assets/types"
+import { PlayArrow,  SkipNext, SkipPrevious, PauseSharp} from '@mui/icons-material'
+import React, { useState, useEffect } from 'react'
+import { Button } from '@mui/material'
+interface Props{
+    song:Song;
+}
+const Player: React.FC<Props> = (props:Props) => {
     const { classes } = useStyles();
-
-
+    //todo: change later to a set state that the app sends
+    const [isPlaying, setisPlaying] = useState<boolean>(false);
+    
+    const startStopSong = (id:string) =>{
+        if(isPlaying){
+            setisPlaying(false)
+        }
+        else{
+            setisPlaying(true)
+        }
+    }
     return (
         <div className={classes.container}>
-            <p>נגן שירים</p>
+            <p className={classes.p}>{ props.song.name }</p>
+            <p className={classes.p}>{ props.song.artist }</p>
+            <div className={classes.buttonsContainer}>
+                <SkipPrevious></SkipPrevious>
+                <Button onClick={() =>startStopSong(props.song.id)}>
+                    {isPlaying &&
+                    <PlayArrow></PlayArrow>
+                    }
+                    {!isPlaying &&
+                    <PauseSharp></PauseSharp>
+                    }
+                </Button>
+                <SkipNext></SkipNext>
+            </div>
         </div>
     )
 }
--- frontend_spotipsy/src/components/Player/playerStyles.tsx
+++ frontend_spotipsy/src/components/Player/playerStyles.tsx
@@ -3,13 +3,20 @@ import { makeStyles } from "tss-react/mui";
 const useStyles = makeStyles()({
     container: {
         backgroundColor: 'rgba(32, 32, 32, 1)',
+        flexDirection: 'column',
         width: '100%',
-        height: '8%',
+        height: '10%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        borderTop: '1px solid rgba(49, 49, 49, 1)'
+        borderTop: '1px solid rgba(49, 49, 49, 1)',
+
+    },buttonsContainer :{
+        height:'50%',
+    },p: {
+        height: '10%',
     },
+
 
 });
 
